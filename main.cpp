e1b41fbb9670040f82595dc6b8f4738c800ea54e
adrienhc <adriennice06@yahoo.fr>
2020-01-26 09:23:02 -0800

    Updated Skybox

--- main.cpp
+++ main.cpp
@@ -13,10 +13,10 @@ int main()
        glfwWindowHint( GLFW_DOUBLEBUFFER, GL_FALSE );
 
    
-    GLFWwindow* window = glfwCreateWindow( WINDOW_WIDTH, WINDOW_HEIGHT, "FPS Game", NULL, NULL);
+    //GLFWwindow* window = glfwCreateWindow( WINDOW_WIDTH, WINDOW_HEIGHT, "FPS Game", NULL, NULL);
                                                                                     //glfwGetPrimaryMonitor()   --for fullscreen app automatically
 
-    //GLFWwindow* window = glfwCreateWindow( WINDOW_WIDTH, WINDOW_HEIGHT, "FPS Game", glfwGetPrimaryMonitor(), NULL);
+    GLFWwindow* window = glfwCreateWindow( WINDOW_WIDTH, WINDOW_HEIGHT, "FPS Game", glfwGetPrimaryMonitor(), NULL);
 
     if (window == NULL)
     {
--- shaders/skyboxShader.fs
+++ shaders/skyboxShader.fs
@@ -6,7 +6,9 @@ out vec4 FragColor;
 
 void main()
 {
-	vec4 col = texture(skybox, TexCoords);
+	FragColor = texture(skybox, TexCoords);
+	float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+    FragColor = vec4(average, average, average, 1.0);
 
-	FragColor = col; 
+	 
 } 
